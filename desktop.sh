8c93eca6d076033da41d90b4f318ce5aabf81b6f
Michał Czyż <michal.czyz35@gmail.com>
2021-12-01 19:56:31 +0100

    Adding submodules

---
+++ desktop.sh
@@ -0,0 +1,1 @@
+sudo stow -t /usr/bin desktop
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
 sudo stow -t /usr/bin bin
 sudo stow -t /usr/lib/systemd/user systemd_units
-sudo cp desktop/multimonitor_prepare /usr/bin/multimonitor_prepare
